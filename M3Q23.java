e2cc7405a9fa0e55fd11ee12ee44d330a71edb32
SKRB <56062164+jerry39313683@users.noreply.github.com>
2019-12-18 16:07:53 +0800

    Create M3Q23.java

---
+++ M3Q23.java
@@ -0,0 +1,33 @@
+import java .util.Scanner;
+public class M3Q23{
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int data[] = new int[n];
+        int j = 0;
+        for(int i = 2 ; i<n ; i++){
+            if(isPrim(i)){
+                data[j]=i;
+                j++;
+            }
+        }
+        for(int i = 1 ; i<j ; i++){
+            for(int k = i+1 ; k<j ; k++){
+                if(data[i]+data[k]==n){
+                    System.out.println(data[i]+"+"+data[k]);
+                }
+            }
+        }    
+    }
+    public static boolean isPrim(int x){
+        boolean flag = true;
+        int i = 2;
+        while(i<x/2){
+            if(x%i==0){
+                flag = false;
+            }
+            i++;
+        }
+        return flag;
+    }
+}
